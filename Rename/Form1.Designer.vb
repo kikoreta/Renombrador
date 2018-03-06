21ca389f490efd094d4a22942ca6909924f3071e
kikoreta <enr.gtz@outlook.com>
2018-03-05 18:58:55 -0600

    v1.0

---
+++ Rename/Form1.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(157, 64)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Renombrar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(48, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(160, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(48, 38)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(42, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(30, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Ruta"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(22, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Ext"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(170, 38)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(38, 20)
+        Me.TextBox3.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(96, 41)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Ext Deseada"
+        '
+        'Button1
+        '
+        Me.Button1.AutoSize = True
+        Me.Button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.Button1.Location = New System.Drawing.Point(214, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(26, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(62, 66)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(89, 20)
+        Me.TextBox4.TabIndex = 10
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 69)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(44, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Nombre"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(243, 91)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.Name = "Form1"
+        Me.Text = "Renombrar Prog"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Rename/Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Public Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim Carpeta = TextBox1.Text
+        Dim ExtArch = "*." + TextBox2.Text
+        Dim Nombre = TextBox4.Text
+        Dim ExtDesead = "." + TextBox3.Text
+        Dim ArchTotal = 0
+        Dim Contador1 As Integer = 0
+        Dim Contador1a As String
+        Dim Contador2 As Integer = 0
+        Dim Contador2a As String
+
+        ArchTotal = Directory.GetFiles(Carpeta, ExtArch).Count
+
+        For Each archivo In My.Computer.FileSystem.GetFiles(Carpeta, FileIO.SearchOption.SearchTopLevelOnly, ExtArch)
+
+            Dim directorio = Path.GetDirectoryName(archivo) + "\"
+            Dim NombArch = Path.GetFileNameWithoutExtension(archivo)
+            Dim arch01 = directorio + Nombre + Mid(NombArch, Nombre.Length + 1) + ExtDesead
+            Contador1 = Contador1 + 1
+
+            Select Case ArchTotal
+                Case Is <= 100
+                    Contador1a = Contador1.ToString("00")
+                Case Is <= 1000
+                    Contador1a = Contador1.ToString("000")
+                Case Is <= 10000
+                    Contador1a = Contador1.ToString("0000")
+                Case Else
+                    Contador1a = Contador1.ToString("0")
+                    Exit Select
+            End Select
+
+            Select Case File.Exists(arch01)
+                Case True
+                    GoTo Cont2
+                Case False
+                    My.Computer.FileSystem.RenameFile(archivo, Nombre + Contador1a + ExtDesead)
+                    GoTo Fin
+            End Select
+
+Cont2:
+            Contador2 = Contador2 + 1
+            Contador2a = Contador2.ToString("00")
+            Dim arch02 = directorio + Nombre + Contador1a + "-" + Contador2a + ExtDesead
+            Select Case File.Exists(arch02)
+                Case True
+                    GoTo Cont2
+                Case False
+                    My.Computer.FileSystem.RenameFile(archivo, Nombre + Contador1a + "-" + Contador2a + ExtDesead)
+                    Contador2 = 0
+            End Select
+Fin:
+        Next
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim browfolder As New FolderBrowserDialog
+        If (browfolder.ShowDialog() = DialogResult.OK) Then
+
+            TextBox1.Text = browfolder.SelectedPath
+        End If
+    End Sub
+
+End Class
